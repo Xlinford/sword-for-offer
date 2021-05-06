6777c0a13c5317cea00f53007ddf1354f9612876
Xlinford <xlinford@gmail.com>
2021-05-07 00:06:47 +0800

    '正则'

--- cha3sec3.py
+++ cha3sec3.py
@@ -24,7 +24,14 @@ class Solution:
         for i in range(1, n):
             print(i)
 
-    # 正则表达式
+    # 正则表达式，pattern为某种规则的正则表达式，s为某字符串，判断pattern是否与s相同
+    # 首先判断模板pattern是否为空；判断首字母是否相同或为'.'，且s不为空；判断pattern长度，大于1且有*则开始递归；
+    #
     def match(self, s, pattern):
         if not pattern: return not s
+        f_match = bool(s) and pattern[0] in {s[0], '.'}
+        if len(pattern) > 1 and pattern[1] == '*':
+            return (self.match(s, pattern[2:])) or (f_match and self.match(s[1:], pattern))
+        else:
+            return f_match and self.match(s[1:], pattern[1:])
 
