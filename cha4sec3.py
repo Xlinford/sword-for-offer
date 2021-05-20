b3921174cf0b2605f1fcfbbb3524a01cbc12cdf7
Linford Xuan <xlinford@163.com>
2021-05-20 23:24:01 +0800

    cha

--- cha4sec3.py
+++ cha4sec3.py
@@ -57,3 +57,43 @@ class Solution:
             if node.right: stack.append(node.right)
 
         return nodelist
+
+    def levelOrderRow(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        nodelist = []
+        stack = [root]
+        while stack:
+            nodelist.append([i.val for i in stack])
+            stack = [j for i in stack for j in (i.left, i.right) if j]
+        return nodelist
+
+    def levelOrderRowZhi(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        nodelist = []
+        stack = [root]
+        flag = -1
+        while stack:
+            nodelist.append([i.val for i in stack])
+            if flag == -1:
+                stack = [j for i in stack for j in (i.right, i.left) if j]
+            else:
+                stack = [j for i in stack[::-1] for j in (i.left, i.right) if j]
+            flag *= -1
+        return nodelist
+
+    def verifyPostorder(self, postorder: List[int]) -> bool:
+        def method(poslist: List[int], start: int, end: int):
+            if start >= end:
+                return True
+            mid = start
+            while poslist[mid] < poslist[end]:
+                mid += 1
+            for i in range(mid, end+1):
+                if poslist[i] < poslist[end]:
+                    return False
+
+            return method(poslist, start, mid-1) and method(poslist, mid, end-1)
+
+        return method(postorder, 0, len(postorder)-1)
