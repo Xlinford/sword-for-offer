6aabecd3fcea4e56b91996acb9a1fc72c9d7f0c9
Xlinford <xlinford@gmail.com>
2021-05-27 00:35:29 +0800

    new sec

--- cha5sec2.py
+++ cha5sec2.py
@@ -12,6 +12,7 @@ class Solution:
 
     def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
         if k >= len(arr): return arr
+
         def sortk(l, r):
             i, j = l, r
             while i < j:
@@ -19,10 +20,11 @@ class Solution:
                 while i < j and arr[i] < arr[l]: i += 1
                 arr[i], arr[j] = arr[j], arr[i]
             arr[i], arr[l] = arr[l], arr[i]
-            if i > k: return sortk(l, i-1)
-            if i < k: return sortk(i+1, r)
+            if i > k: return sortk(l, i - 1)
+            if i < k: return sortk(i + 1, r)
             return arr[:k]
-        return sortk(0, len(arr)-1)
+
+        return sortk(0, len(arr) - 1)
 
     class MedianFinder:
 
@@ -42,26 +44,44 @@ class Solution:
                 heappush(self.A, -heappop(self.B))
 
         def findMedian(self) -> float:
-            return self.A[0] if len(self.A) != len(self.B) else (self.A[0]-self.B[0])/2
+            return self.A[0] if len(self.A) != len(self.B) else (self.A[0] - self.B[0]) / 2
 
     def maxSubArray(self, nums: List[int]) -> int:
         for i in range(1, len(nums)):
-            if nums[i-1] > 0:
-                nums[i] += nums[i-1]
+            if nums[i - 1] > 0:
+                nums[i] += nums[i - 1]
         return max(nums)
+
     def maxSubArray1(self, nums: List[int]) -> int:
         max = nums[0]
         dp = nums[0]
         for i in range(1, len(nums)):
             if dp > 0:
                 dp += nums[i]
-            else: dp = nums[i]
+            else:
+                dp = nums[i]
             if dp > max:
                 max = dp
         return max
 
     def countDigitOne(self, n: int) -> int:
         digit, res = 1, 0
-        high, cur, low = n//10, n%10, 0
-        while high!=0 or cur!=0:
-            if cur==0:res +=
+        high, cur, low = n // 10, n % 10, 0
+        while high != 0 or cur != 0:
+            if cur == 0:
+                res += high * digit
+            elif cur == 1:
+                res += high * digit + low + 1
+            else:
+                res += (high + 1) * digit
+            low += cur * digit
+            cur = high % 10
+            high //= 10
+            digit *= 10
+        return res
+
+    def findNthDigit(self, n: int) -> int:
+        digit, start, count = 1, 0, 0
+        while n > count:
+            n -= count
+            digit += 1
